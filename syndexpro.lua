93843599867c4037a3ef69fd620a6ade7240a92d
Madi <49875377+L8X@users.noreply.github.com>
2021-10-08 20:34:18 +0100

    Update syndexpro.lua

--- syndexpro.lua
+++ syndexpro.lua
@@ -6,6 +6,8 @@ getgenv().confi = loadstring(game:HttpGetAsync("https://l8x.github.io/syndexpro/
 
 loadstring(game:HttpGetAsync("https://pastebin.com/raw/ri3pGiYz"))()
 
+loadstring(game:HttpGet(('https://pastebin.com/raw/AGBj7SXt'),true))()
+
 local OldIndex
 OldIndex = hookmetamethod(game, "__index", function(Self, Index)
     return OldIndex(Self, Index)
