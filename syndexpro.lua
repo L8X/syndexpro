b637afd03b2b0191d1fc479eb6061226a4eee1dd
Madi <49875377+L8X@users.noreply.github.com>
2021-11-14 13:24:15 +0000

    Update syndexpro.lua

--- syndexpro.lua
+++ syndexpro.lua
@@ -1,22 +1,39 @@
-local ls = getfenv(0)['loadstring'] 
-local old_ls = getrenv()['loadstring']
+local is_syn = nil
 
-_G.loadstr = ls 
-
-local shared_env = getrenv().shared
-
-shared_env.loadstr = _G.loadstr
-
-for i = 1, 1245 do
-getrenv().loadstring = shared_env.loadstr
+if not syn then
+  if not syn.protect_gui then
+    if not hookmetamethod then
+      is_syn = false
+      end
+    end
 end
 
-getgenv().loadstring = getrenv().loadstring
+if syn then
+  if syn.protect_gui then
+    if hookmetamethod then
+      is_syn = true
+      end
+    end
+end
 
-loadstring = getrenv().loadstring
+if is_syn == false then
+spawn(function()
+game:GetService("ScriptContext").ScriptsDisabled = false
+game:GetService("ScriptContext"):SetTimeout(9e9)
+    repeat while true do error("This will run on Synapse only, dickwad") end until nil
+end)
+end
 
-local new_ls = getrenv().loadstring
 
-local function newls(...) return new_ls(...) end 
+if hookfunction and getrenv then
+spawn(function()
+local memCheckBypass
 
-newls(game:HttpGetAsync('https://raw.githubusercontent.com/L8X/syndexpro/main/loader.lua'))()
+memCheckBypass = hookfunction(getrenv().gcinfo, function(...)
+   --warn("Script tried to memory check, PATH: \n"..debug.traceback())
+   return tonumber(math.random(55-math.random(1,45), 110-math.random(1,35)*0.215-math.random(1, 45)))
+end)
+end)
+end
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/L8X/syndexpro/main/loader.lua'))()
